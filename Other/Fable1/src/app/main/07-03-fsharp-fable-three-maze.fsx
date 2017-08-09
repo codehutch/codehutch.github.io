7775305427f9c125c4d699a07231b55cd9a87d11
Andy Hutchinson <codehutch@users.noreply.github.com>
2017-08-09 21:42:04 +0100

    compress and attempt to add spinning

--- Other/Fable1/src/app/main/07-03-fsharp-fable-three-maze.fsx
+++ Other/Fable1/src/app/main/07-03-fsharp-fable-three-maze.fsx
@@ -32,16 +32,6 @@ literals. However, whilst ascii art is undeniably masssively cool, using strings
 
 *)
 
-// Option 1 - Use strings and ascii art - cool but not cool
-
-let stringMazes = """ 
-
-     +-+                      +-+                                   +-+-+
-     | | <= 1x1 square box    |   <= very simple 1x1 square maze    |     <= really complicated
-     +-+                      + +                                   + +-+    2x2 square maze
-                                                                        |
-                                                                    +-+-+
-"""
 
 (**
 
@@ -448,72 +438,45 @@ let rec randomMaze n =
                    (TopReq None)
                    (LeftReq None)]]
                
-let width () = Browser.window.innerWidth * 0.75;
-let height () = Browser.window.innerHeight * 0.5
+
 
 (**
 
-#### _**1:** Lights_ ####
+#### _**5:** Action_ ####
 
-Our scene needs lights (so that we can see stuff).The below function adds a dim ambient light and
-a bright spotlight to the scene. Ambient light defines a base-level of illumination within the
-scene. It does not have a particular direction or position (hence it is easy to create). By contrast,
-a spotlight has an exact location and shines in a particular direction, illuminating objects within
-it's beam differently depending on the angle they meet it at. Here we just set the spotlight's
-position and leave it shining at the origin (the center of our scene). Note that before we
-specify a colour as a hex string, we have to use `U2.Case2` to create a union case. This is because
-the underlying javascript libraries are weakly typed, but F# is very much a strongly typed language.
-Therefore, where javascript functions are willing to accept various types of arguments, the F#
-translation of them has to wrap each of the javascript-acceptable types in a union to keep the F#
-type-system happy.
+Finally we're there. We can create a Scene and initialise all required elements by calling
+the functions we defined above. We return a 4-tuple of the 4 key graphics elements back to
+the caller so that those elements can be used later on in rendering / animation. In-fact,
+"the caller" is just the line of script at the bottom of the section, which creates top-level
+bindings to each of the key graphics elements.
 
 *)
 
-let initLights (scene:Scene) =
+let mutable spin = false
 
-    let ambientLight = Three.AmbientLight(U2.Case2 "#3C3C3C", 1.0)
-    scene.add(ambientLight)
+let action() =
 
-    let spotLight = Three.SpotLight(U2.Case2 "#FFFFFF")
-    spotLight.position.set(-30., 60., 60.) |> ignore
-    scene.add(spotLight)
+    let width () = Browser.window.innerWidth * 0.75;
+    let height () = Browser.window.innerHeight * 0.5
 
-(**
-
-### _**2:** Camera_ ###
-
-We also need a camera, which sets the location that the scene is viewed from. We need to set the
-aspect ratio of the camer'a field of view. We use the `width()` and `height()` functions we defined above so that the
-camera field's aspect will match the intended dimensions of our graphics output area. The last line of
-the function is its return value; so the camera is returned back to the caller so that it can be used
-during rendering.
-
-*)
-
-let initCamera () =
+    let scene = Three.Scene()
+    scene.autoUpdate <- true
 
     let camera = Three.PerspectiveCamera(75.0, width() / height(), 0.01, 1000.0)
+
     camera.matrixAutoUpdate <- true
     camera.rotationAutoUpdate <- true
     camera.position.z <- 2.0
-    camera
 
-(**
+    let initLights () =
+      
+      scene.add(Three.AmbientLight(U2.Case2 "#3C3C3C", 1.0))
 
-#### _**3:** Renderer_ ####
+      let spotLight = Three.SpotLight(U2.Case2 "#FFFFFF")
+      spotLight.position.set(-30., 60., 60.) |> ignore
+      scene.add(spotLight)
 
-OK, so not quite a case of _lights-camera-action_, as we now need a renderer. A renderer is sort of
-analogous to a screen, and embodies the output area for our graphics. We have to tell Three which
-DOM element within our page to put the render target into (and our HTML page must contain an element
-called `graphicsContainer`. We also get to choose which kind of renderer to use. `WebGLRenderer` is
-the fastest, but Three does support other renderers that could be used on devices without WebGL support.
-The call to `setClearColour` sets the background colour for areas of the screen that are not otherwise
-drawn on. Again we set the size of the output area using the `width()` and `height()` functions that we defined
-above, so that the output dimensions tie up with the aspect ratio of the camera.
-
-*)
-
-let initRenderer (scene:Scene) =
+    initLights ()
 
     let renderer = Three.WebGLRenderer()
     renderer.setClearColor("#0A1D2D")
@@ -533,10 +496,15 @@ let initRenderer (scene:Scene) =
         button.className <- cssClass
 
         let buttonClick (b : Browser.MouseEvent) =
-            while(scene.children.Count > 0) do 
-                scene.remove(scene.children.Item(0)) 
-            initLights scene
-            renderMaze scene -1.025 1.15 1.275 -1.15 <| randomMaze difficulty
+            spin <- true
+            Async.Start (async {
+                do! async { while(scene.children.Count > 0) do 
+                              scene.remove(scene.children.Item(0)) 
+                            initLights () } 
+                let! maze = async { return randomMaze difficulty } 
+                renderMaze scene -1.025 1.15 1.275 -1.15  maze |> ignore
+                spin <- false
+            }) |> ignore 
             (Boolean() :> obj)
 
         button.onclick <- Func<_,_> buttonClick
@@ -549,35 +517,13 @@ let initRenderer (scene:Scene) =
     buttonContainer.appendChild(makeButton "Medium" 3 "blueViolet") |> ignore
     buttonContainer.appendChild(makeButton "Hard" 4 "blueGreen") |> ignore
     buttonContainer.appendChild(makeButton "Crazy" 5 "yellowGreen") |> ignore
-    buttonContainer.appendChild(makeButton "Insane 05" 6 "yellowGreen") |> ignore
+    buttonContainer.appendChild(makeButton "Insane 17" 6 "yellowGreen") |> ignore
 
     let solveButton = Browser.document.createElement("button")
     solveButton.innerText <- "Solve!"
     solveButton.className <- "yellowOrange" 
     buttonContainer.appendChild(solveButton) |> ignore
 
-    renderer
-
-(**
-
-#### _**5:** Action_ ####
-
-Finally we're there. We can create a Scene and initialise all required elements by calling
-the functions we defined above. We return a 4-tuple of the 4 key graphics elements back to
-the caller so that those elements can be used later on in rendering / animation. In-fact,
-"the caller" is just the line of script at the bottom of the section, which creates top-level
-bindings to each of the key graphics elements.
-
-*)
-
-let action() =
-
-    let scene = Three.Scene()
-    scene.autoUpdate <- true
-
-    initLights scene
-    let camera = initCamera ()
-    let renderer = initRenderer scene
     renderMaze scene -1.025 1.15 1.275 -1.15 (randomMaze 3) 
 
     renderer, scene, camera
@@ -596,14 +542,15 @@ on screen.
 
 *)
 
-let render() =
-    renderer.render(scene, camera)
 
 let rec reqFrame (dt:float) =
     Browser.window.requestAnimationFrame(Func<_,_> animate) |> ignore
-    render()
+    if spin 
+    then camera.rotateZ ( 0.07 ) |> ignore
+    else camera.rotation.z <- 0.0
+    renderer.render(scene, camera)
 and animate (dt:float) =
-    Browser.window.setTimeout(Func<_,_> reqFrame, 1000.0 / 30.0) |> ignore // 30 fps
+    Browser.window.setTimeout(Func<_,_> reqFrame, 1000.0 / 20.0) |> ignore // aim for 20 fps
 
 animate(0.0) // Start the animation going
 

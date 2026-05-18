4c5c504ce5efb1a552a3e7ad28bd186da8ca9d35
codehutch <andy@hutch>
2026-05-18 21:21:10 +0100

    update About 3d H

--- astro/src/components/AboutScene.jsx
+++ astro/src/components/AboutScene.jsx
@@ -7,19 +7,20 @@ export default function App() {
     <div id="canvas-container">
 		<Canvas>
         	<OrbitControls 
-				rotateSpeed={2}
+				rotateSpeed={1}
 				autoRotate={true}
-				autoRotateSpeed={5}			
+				autoRotateSpeed={15}			
 			/>			
-			<mesh position={[-2  , -2, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[-2  , 0, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[-2  , 2, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[0   , 0, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[2   , -2, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[2   , 0, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
-			<mesh position={[2   , 2, 0]}><boxGeometry args={[2, 2, 2]} /><meshPhongMaterial /></mesh>
+			<mesh position={[-1.25 , -1.25, 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[-1.25 ,  0   , 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[-1.25 ,  1.25, 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[ 0    ,  0   , 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[ 1.25 , -1.25, 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[ 1.25 ,  0   , 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
+			<mesh position={[ 1.25 ,  1.25, 0]}><boxGeometry args={[1.25, 1.25, 1.25]} /><meshPhongMaterial /></mesh>
 			<ambientLight intensity={0.1} />
 			<directionalLight position={[5, 4, 3]} intensity={0.95} color="cyan" />
+			<directionalLight position={[-5, -4, -3]} intensity={0.55} color="magenta" />			
 		</Canvas>
     </div>
   )

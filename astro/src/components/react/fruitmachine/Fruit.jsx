799fda2d903b497bbfeb862c47fbbe4d2ba90393
codehutch <andy@hutch>
2026-06-04 13:04:47 +0100

    fruit machine tweaks and start adding blog text

--- astro/src/components/react/fruitmachine/Fruit.jsx
+++ astro/src/components/react/fruitmachine/Fruit.jsx
@@ -1,18 +1,15 @@
 export default function Fruit({ icon, rotation }) {
 
-  let scale = 3.8;
-
   let rotateMod = rotation % 360;
 
-  let myStyle = {
-    fontSize:scale + 'rem',
+  let fruitStyle = {
+    fontSize:'3.8rem',
     position: 'absolute',
     backfaceVisibility: 'hidden',
     transform: 'translateY(88px) rotateX(' + rotateMod + 'deg) translateZ(135px)',
   };
 
   return (
-    <span style={myStyle} className="relative aspect-square safari-text-shift-right-5">{icon}</span>
+    <span style={fruitStyle} className="relative aspect-square safari-text-shift-right-5">{icon}</span>
   );
-
 }
